5f1b76ea37c9b59bcfd184515bf4deaff7b0bc62
Gabriel Faria <gabriel@goncalvesfaria.com>
2022-05-21 17:01:36 -0300

    Props / Downloaded CSS / using variable inside JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+  const expenses = [
+    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: "Cat food", amount: 150.37, date: new Date(2021, 5, 13) },
+    { title: "Clothes", amount: 2580.40, date: new Date(2021, 3, 18) },
+    { title: "Library", amount: 5.36, date: new Date(2021, 2, 15) },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
