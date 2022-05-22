850e5b9ef044080bd582b6d04ba5ddeb4aab021c
Gabriel Faria <gabriel@goncalvesfaria.com>
2022-05-22 20:01:27 -0300

    Add multiple states to listen to multiple user inputs

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,25 +1,58 @@
-import './ExpenseForm.css'
+import { useState } from "react";
+
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    return <form>
-        <div className="new-expense__controls">
-            <div className="new-expense__control">
-                <label>Title</label>
-                <input type='text' />
-            </div>
-            <div className="new-expense__control">
-                <label>Amount</label>
-                <input type='number' min="0.01" step="0.01" />
-            </div>
-            <div className="new-expense__control">
-                <label>Date</label>
-                <input type='date' min="2019-01-01" max="2022-12-31" />
-            </div>
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleClickHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    console.log(enteredTitle);
+  };
+
+  const amountClickHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    console.log(enteredAmount);
+  };
+
+  const dateClickHandler = (event) => {
+    setEnteredDate(event.target.value);
+    console.log(enteredDate);
+  };
+
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleClickHandler} />
         </div>
-        <div className="new-expense__actions">
-            <button type="submit">Add Expense</button>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountClickHandler}
+          />
         </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateClickHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
     </form>
+  );
 };
 
 export default ExpenseForm;
